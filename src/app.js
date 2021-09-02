664bf7ad7683a8962d7be317b524fc7fb4929b50
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-09-02 22:00:13 +0530

    implemented Patch req

--- src/app.js
+++ src/app.js
@@ -16,6 +16,28 @@ app.post("/students", async (req, res) => {
   }
 });
 
+app.get("/students", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    res.send(studentsData);
+    console.log(studentsData);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.patch("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const updatedData = await Student.findByIdAndUpdate(_id, req.body);
+    console.log(req.body);
+    res.send(updatedData);
+    console.log(updatedData);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 app.listen(port, () => {
   console.log(`connection is setup at ${port}`);
 });
